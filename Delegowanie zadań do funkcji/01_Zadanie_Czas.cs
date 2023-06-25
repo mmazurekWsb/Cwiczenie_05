c40d7dd059afa8a03ec92eb58401c48c1744de5b
mmazurekWsb <chx100030@student.chorzow.wsb.pl>
2023-06-25 11:00:25 +0200

    .

---
+++ Delegowanie zadań do funkcji/01_Zadanie_Czas.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _01_Zad
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int czas = 332;
+            string sformatowanyCzas = FormatujCzas(czas);
+            Console.WriteLine(sformatowanyCzas); // Wyświetli: 00:05:32
+            Console.ReadKey();
+        }
+
+        static string FormatujCzas(int czas)
+        {
+            int godziny = czas / 3600;
+            int minuty = (czas % 3600) / 60;
+            int sekundy = czas % 60;
+
+            return $"{godziny:D2}:{minuty:D2}:{sekundy:D2}";
+        }
+    }
+}
---
+++ Delegowanie zadań do funkcji/_02ZadanieDlugoscOdcinka.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _02Zadanie
+{
+    internal class _02ZadanieDlugoscOdcinka
+    {
+        static void Main(string[] args)
+        {
+            double x1 = 3.5;
+            double y1 = 2.0;
+            double x2 = 8.2;
+            double y2 = 4.7;
+
+            double dlugoscOdcinka = ObliczDlugoscOdcinka(x1, y1, x2, y2);
+            Console.WriteLine("Długość odcinka: " + dlugoscOdcinka);
+            Console.ReadKey();
+        }
+
+        static double ObliczDlugoscOdcinka(double x1, double y1, double x2, double y2)
+        {
+            double deltaX = x2 - x1;
+            double deltaY = y2 - y1;
+            double dlugosc = Math.Sqrt(deltaX * deltaX + deltaY * deltaY);
+            return dlugosc;
+        }
+    }
+}
+
+        
+ 
